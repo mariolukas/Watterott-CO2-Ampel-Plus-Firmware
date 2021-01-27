236645c05fbb80a261c2715c1c041c05d7c8794d
Andreas Watterott <1488433+awatterott@users.noreply.github.com>
2021-01-27 14:30:38 +0100

    Update NetworkManager.cpp

--- CO2-Ampel_Plus/NetworkManager.cpp
+++ CO2-Ampel_Plus/NetworkManager.cpp
@@ -96,7 +96,7 @@ int wifi_wpa_connect() {
   } else {
     print_wifi_status();
     server.begin();
-    if (!strcmp(cfg.mqtt_broker_address, "127.0.0.1")) {
+    if (strcmp(cfg.mqtt_broker_address, "127.0.0.1")) { /* prevent connection to localhost */
       mqtt_connect();
     }
   }
