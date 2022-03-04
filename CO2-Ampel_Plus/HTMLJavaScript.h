073b08b6c904d6980dd9806dffae5fd5a10d9948
Bernhard Tittelbach <bernhard@tittelbach.org>
2022-03-04 20:36:56 +0100

    show ASC status in webinterface

--- CO2-Ampel_Plus/HTMLJavaScript.h
+++ CO2-Ampel_Plus/HTMLJavaScript.h
@@ -17,7 +17,7 @@ function refreshAmpel() {
         if (this.readyState == 4 && this.status == 200) {
             var state = JSON.parse(this.responseText);
             clearAmpel();
-            document.getElementById("state").innerHTML = "<b>Co2:</b> "+state.co2+" ppm<br><b>Temperature:</b> "+state.temp + " &ordm;C<br><b>Illumination:</b> "+state.lux+" LUX <br><b>Humidity:</b> "+state.hum+" %<br><b>MQTT connected:</b> "+!!state.mqtt+"<br><br>Firmware: "+state.firmware;
+            document.getElementById("state").innerHTML = "<b>Co2:</b> "+state.co2+" ppm<br><b>Temperature:</b> "+state.temp + " &ordm;C<br><b>Illumination:</b> "+state.lux+" LUX <br><b>Humidity:</b> "+state.hum+" %<br><b>MQTT connected:</b> "+!!state.mqtt+"<br><b>CO2 AutoCalibration:</b> "+(!!state.co2ASC)+"<br><br>Firmware: "+state.firmware;
             if (state.co2 < 800) {
                el.classList.add("ampelgruen");
             } else if (state.co2 < 1000) {
